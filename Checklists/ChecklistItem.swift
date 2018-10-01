d84521ef5ee1bc74bb3e834cf85d66bda37cf3ae
Leo <leo.a.rubiano@gmail.com>
2018-10-01 13:25:47 -0400

    created ChecklistItem class and replaced variables with instances of class and its properties

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Leonardo Rubiano on 01.10.18.
+//  Copyright © 2018 leofofeo. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
