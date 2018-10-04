e7befc5d2cb97858785bb8733495f4a41323512b
Leo <leo.a.rubiano@gmail.com>
2018-10-04 17:39:46 -0400

    added text validation to additemviewcontroller

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
-
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
     
     @IBOutlet weak var textField: UITextField!
-    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +24,28 @@ class AddItemViewController: UITableViewController {
         return nil
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(String(describing: textField.text))")
         navigationController?.popViewController(animated: true)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        
+        return true
     }
 
 }
