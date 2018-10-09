b6cecfd3b8b251c90439f031edbf7188e6440078
Leo <leo.a.rubiano@gmail.com>
2018-10-09 16:29:23 -0500

    data model stuff

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -49,14 +49,10 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         lists = loadDummyData()
-        
-        for list in lists {
-            let item = ChecklistItem()
-            item.text = "Item for \(list.name)"
-            list.items.append(item)
-        }
-        
+    
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        loadChecklists()
     }
 
     override func didReceiveMemoryWarning() {
@@ -143,6 +139,44 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         lists.append(list)
         
         return lists
+    }
+    
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+
+        let encoder = PropertyListEncoder()
+
+        do {
+            
+            let data = try encoder.encode(lists)
+            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
+            
+        } catch {
+            print("Error encoding item array!")
+        }
+    }
+    
+    func loadChecklists(){
+    
+        let path = dataFilePath()
+    
+        if let data = try? Data(contentsOf: path){
+            let decoder = PropertyListDecoder()
+            do {
+                
+                lists = try decoder.decode([Checklist].self, from: data)
+            } catch {
+                print ("Error decoding item array!")
+            }
+        }
     }
  
 
