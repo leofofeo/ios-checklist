b8534365f8996d7978cff04e65dbf0675ec7c37f
Leo <leo.a.rubiano@gmail.com>
2018-10-09 14:12:20 -0500

    editing checklists

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -99,6 +99,15 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
             controller.delegate = self
         }
     }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
+        controller.delegate = self
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+        
+        navigationController?.pushViewController(controller, animated: true)
+    }
 
     func makeCell(for tableView: UITableView) -> UITableViewCell {
         let cellIdentifier  = "Cell"
